58dccaf9f1158d80e34eacfbedf2e9e3b8205338
lawrencelj <Lawrencesubscribe@gmail.com>
2014-11-22 22:33:43 +1100

    Completed Draft of Make CacheMatrix
    completed draft for all functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,68 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Jian Liang completed on 22nd Nov 2014 for R Programming Assignment 2
 
 ## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  if(!is.na(x)){
-    Inverse <- solve(x)
+  ## Initialized inverse matrix
+  Inverse <- NULL
+  
+  ## This function is to initialized a matrix, if passed
+  ## in matrix is NULL or not Matrix class object, a 1 value
+  ## matrix will be store , and a warning message will
+  ## be given. other
+  setm <- function(mat){
+    if(class(mat)=="matrix" & mat!=NULL){
+      x<<-mat
+      Inverse <<-NULL
+    }
+    else{
+      warning("Please ensure you pass a matrix class valuable as parameter.")
+    }
   }
+  
+  getm <- function(){
+    x
+  }
+  
+  ## This function is to store inverse matrix when origional
+  ## matrix and passed in matrix are both not empty. It doesn't check
+  ## if a matrix is inversible. When assign successfully, it
+  ## will return TRUE, otherwise FALSE
+  setinvm <- function(inv){
+    if(is.na(x)){
+      warning("There is no matrix initialized yet, please provid a matrix
+              before given a inverse matrix")
+      return FALSE
+    }
+    else if (is.na(inv)){
+      warning("Empty matrix is not acceptable as inverse matrix")
+      return FALSE
+    }
+    Inverse<<-inv
+    return TRUE
+  } 
+  
+  ## If there is no initialized matrix, a NULL value will
+  ## be returned.
+  getinvm <- Function(){
+    if(!is.na(x)){
+      Inverse <<- solve(x)
+      return Inverse
+    }
+    else{
+      warning("Please use setm() to assign a non-NA matrix")
+      return NULL
+    }
+  }
+  
+  list(setm = setm, getm = getm,
+        setinvm = setinvm,
+        getinvm = getinvm)
+    
+    
+  }
+  
+  
   
 
 }
